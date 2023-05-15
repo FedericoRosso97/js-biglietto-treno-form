6bccc09c055f97500c5a5922d1990401c5e25f3a
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-15 17:04:07 +0200

    stampo lo sconto e arrotondo prezzo a 2 decimali

--- node.js
+++ node.js
@@ -4,17 +4,19 @@ const input=document.getElementById('distance');
 const button=document.getElementById('bottone');
 
   button.addEventListener("click", function() {
-    distance=input.value;
-    age=inputAge.value;
+    let distance=input.value;
+    let age=inputAge.value;
     let prezzo=distance * 0.233;
-    console.log(prezzo);
+    
+    console.log(age);
+    console.log(distance);
     
     if(age<18){
         let sconto=(19.4/100)*distance;
-        prezzo=(distance*0.233)-sconto + 'sconto 19%';
+        prezzo=(distance*0.233)-sconto +''+'sconto 19%';
       }else if(age>65){
         let sconto=(37.7/100)*distance;
-        prezzo=(distance*0.233)-sconto + 'sconto 37%';
+        prezzo=(distance*0.233)-sconto +''+'sconto 37%';
       }
-
+   console.log(prezzo.toFixed(2)+'$')
   });
