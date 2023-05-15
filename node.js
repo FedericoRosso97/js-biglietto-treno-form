7aee09ade5d80ef957bec56456f66ce091934a7a
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-15 18:46:54 +0200

    inizio a cambiare colore al biglietto

--- node.js
+++ node.js
@@ -1,22 +1,46 @@
 const inputAge=document.getElementById('user-age');
-const input=document.getElementById('distance');
+const inputDistance=document.getElementById('distance');
 
 const button=document.getElementById('bottone');
-
-  button.addEventListener("click", function() {
-    let distance=input.value;
+const recap=document.querySelector('div.recapi');
+  
+    button.addEventListener("click", function() {
+    let distance=inputDistance.value;
     let age=inputAge.value;
     let prezzo=distance * 0.233;
-    
+
     console.log(age);
     console.log(distance);
     
     if(age<18){
         let sconto=(19.4/100)*distance;
-        prezzo=(distance*0.233)-sconto +''+'sconto 19%';
+        prezzo=(distance*0.233)-sconto;
       }else if(age>65){
         let sconto=(37.7/100)*distance;
-        prezzo=(distance*0.233)-sconto +''+'sconto 37%';
+        prezzo=(distance*0.233)-sconto;
       }
-   console.log(prezzo.toFixed(2)+'$')
+      console.log(prezzo);
+      let prezzoArrotondato=prezzo.toFixed(2)
+      recap.classList.add('visible');
+      recap.classList.remove('invisible');
+
+      const Eta=document.querySelector('p.età')
+      Eta.innerHTML +=age+' '+'anni!';
+
+      const km=document.querySelector('p.distanza')
+      km.innerHTML +=distance+' '+'chilometri!';
+
+      const soldi=document.querySelector('p.price')
+      soldi.innerHTML +=prezzoArrotondato+' '+'euro!';
+
+      const eta=document.querySelector('p.eta')
+      eta.innerHTML +=age+' '+'anni!';
+
+      const Km=document.querySelector('p.Distanza')
+      Km.innerHTML +=distance+' '+'chilometri!';
+
+      const Soldi=document.querySelector('p.Price')
+      Soldi.innerHTML +=prezzoArrotondato+' '+'euro!';
+
   });
+
