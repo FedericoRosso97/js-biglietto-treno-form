dff34be6b690afd236162b0772aa9a09d75c6bb3
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-15 16:44:20 +0200

    sposto il codice sconto dentro addEventListener

--- node.js
+++ node.js
@@ -1,20 +1,20 @@
-const age=parseInt(document.getElementById('user-age'));
+const inputAge=document.getElementById('user-age');
 const input=document.getElementById('distance');
-
-
 
 const button=document.getElementById('bottone');
 
-  if(age<18){
-    let sconto=(19.4/100)*distance;
-    prezzo=(distance*0.233)-sconto;
-  }else if(age>65){
-    let sconto=(37.7/100)*distance;
-    prezzo=(distance*0.233)-sconto;
-  }
-
   button.addEventListener("click", function() {
     distance=input.value;
+    age=inputAge.value;
     let prezzo=distance * 0.233;
     console.log(prezzo);
+    
+    if(age<18){
+        let sconto=(19.4/100)*distance;
+        prezzo=(distance*0.233)-sconto;
+      }else if(age>65){
+        let sconto=(37.7/100)*distance;
+        prezzo=(distance*0.233)-sconto;
+      }
+
   });
